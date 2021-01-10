a9ac27f600a9d74887ef21f36169b68e0d795551
minhajspace <minhaj.space@gmail.com>
2021-01-10 12:03:21 +0530

    fairebase setup file

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+    // Your web app's Firebase configuration
+  var firebaseConfig = {
+        apiKey: "AIzaSyBUHCddrMDPI07gVK325wtqXwFXONGw0c0",
+    authDomain: "login-70448.firebaseapp.com",
+    projectId: "login-70448",
+    storageBucket: "login-70448.appspot.com",
+    messagingSenderId: "131700802380",
+    appId: "1:131700802380:web:333457a547a718ab4c85e9"
+  };
+  // Initialize Firebase
+  const fire = firebase.initializeApp(firebaseConfig);
+  export default fire;
